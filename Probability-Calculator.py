15464700ed9012c8f34fd4344ed3e4d55adeb946
Fran Panteli <francescapanteli@gmail.com>
2024-04-25 15:39:48 +0100

    This commit pushes changes to the project Task Challenge Notes.pdf file.

--- Probability-Calculator.py
+++ Probability-Calculator.py
@@ -1,16 +1,28 @@
+# Importing modules 
 import copy
 import random
-# Consider using the modules imported above.
-
 
 class Hat:
+
+"""
+    Defining the hat class: 
+        -> This is the class which we use to create hats 
+        -> Each of those hats contains balls 
+        -> The __init__ method <- This is the method which allows us to set the number of different balls 
+            of each colour in the hat
+        -> We have a number of balls and the colour of each type of ball 
+        -> This method takes this and puts it into a list for the hat 
+        -> We are taking the colour and repeating it in the list by the frequency we have been given for 
+            that colour 
+        -> Then doing this for all of the balls in the hat
+"""
+
     def __init__(self, **kwargs):
         self.contents = [k for k, v in kwargs.items() for _ in range(v)]
 
     def draw(self, n):
         n = min(n, len(self.contents))
         return [self.contents.pop(random.randrange(len(self.contents))) for _ in range(n)]
-
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     m = 0
