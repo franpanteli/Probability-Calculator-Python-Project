a5d8c3d4922d8a2212a74e4df60ff7ba7ec03834
Fran Panteli <francescapanteli@gmail.com>
2024-04-25 18:43:53 +0100

    This commit pushes changes to the project Task Challenge Notes.pdf file.

--- Probability-Calculator.py
+++ Probability-Calculator.py
@@ -20,9 +20,38 @@ class Hat:
     def __init__(self, **kwargs):
         self.contents = [k for k, v in kwargs.items() for _ in range(v)]
 
+"""
+    Defining the draw function:
+        -> This method allows us to draw balls from the hat which we just initialised 
+        -> We need to make sure that the number of balls which we are taking out of the hat isn't greater 
+            than the number of balls which we have to choose from 
+        -> We take the hat with the list of balls in it and remove a random selection of those balls, and 
+            return a list of length `n` with these
+        -> So each time this function is run on the balls in a hat, it returns a different selection of them 
+        -> Rather than using the expected value of the probability, we can run this function multiple times 
+            for an experiment 
+        -> This is the next function
+"""
+
     def draw(self, n):
         n = min(n, len(self.contents))
         return [self.contents.pop(random.randrange(len(self.contents))) for _ in range(n)]
+
+"""
+    Defining the experiment function:
+        -> This function is defined outside of the `Hat` class
+        -> We are running the draw function multiple times and creating a counter to track the number of 
+            successful experiments
+        -> Each time we run the draw function, we can calculate if the result matches its expected value 
+        -> If it does, then we increment the value of a counter by 1
+        -> Then this repeated multiple times by the function <- The more it is then the more accurate the 
+            outcome 
+        -> Then we take the counter as a percentage of all of the experiments run 
+        -> This allows us to simulate a distribution of sample means for a given bag with a certain number 
+            of balls in it 
+        -> The counter is only incremented if the output of this matches the expected probability for all 
+            of the colours in the hat 
+"""
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     m = 0
